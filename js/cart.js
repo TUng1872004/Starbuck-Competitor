a3c78f39507e5143f56609228b03ac61f5800263
mqthanggg <maiquocthang0304@gmail.com>
2025-11-19 11:17:37 +0700

    update cart

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,72 @@
-// document.querySelectorAll('.remove_item').forEach(btn => {
-//     btn.addEventListener('click', function() {
-//         this.closest('tr').remove();
-//     });
-// });
+let qrNavigation = null
+
+fetch(
+    './data/cart.json'
+).then(
+    data => data.json()
+).then(
+    data => {
+        let totalPrice = 0
+        const cartListElement = document.querySelector('tbody')
+        for (const item of data){
+            cartListElement.innerHTML += `
+                <tr item-id="${item.id}">
+                    <td><img src="${item.image}" class="cart_item_img"></td>
+                    <td>${item.name}</td>
+                    <td class="item-price" price="${item.price}">${item.price}₫</td>
+                    <td><input type="number" value="${item.quantity}" min="1" class="qty_input"></td>
+                    <td class="total-price">${item.price*item.quantity}₫</td>
+                    <td><i class="fa fa-trash-o remove_item"></i></td>
+                </tr>
+            `
+            totalPrice += item.price * item.quantity
+        }
+        document.querySelector('.summary-price').textContent = totalPrice + '₫'
+        document.querySelectorAll('.qty_input').forEach(tr => {
+            tr.addEventListener('change', function(){
+                const row = this.closest('tr')
+                const id = Number(row.getAttribute('item-id'))      
+                totalPrice -= Number(row.querySelector('.total-price').textContent.slice(0,row.querySelector('.total-price').textContent.length-1))                
+                const itemPrice = Number(row.querySelector('.item-price').getAttribute('price'))
+                const newPrice = Number(this.value) * itemPrice
+                const itemIndex = data.findIndex((item) => {
+                    return item.id === id
+                })                
+                data[itemIndex].quantity = Number(this.value)
+                row.querySelector('.total-price').textContent = newPrice + '₫'
+                totalPrice += newPrice
+                document.querySelector('.summary-price').textContent = totalPrice + '₫'
+            })
+        })
+        document.querySelectorAll('.remove_item').forEach(btn => {
+            btn.addEventListener('click', function() {
+                
+                const id = Number(this.closest('tr').getAttribute('item-id'));
+                const itemIndex = data.findIndex((item) => {
+                    return item.id === id
+                })
+                data = data.splice(itemIndex, 1)
+                
+                fetch("http://localhost:5000/api/remove-cart", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(data),
+                });
+            });
+        });
+
+        qrNavigation = () => {
+            fetch("http://localhost:5000/api/remove-cart", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({}),
+            })
+            .then(
+                _ => {
+                    window.location.href = '/qr.html'
+                }
+            );
+        }
+    }
+)
 
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(express.json()); // parse JSON bodies
 
 const PRODUCTS_FILE = path.join(__dirname, "data", "products.json");
 
+const CART_FILE = path.join(__dirname, "data", "cart.json");
+
 // Helper to read/write products.json
 function readProducts() {
     return JSON.parse(fs.readFileSync(PRODUCTS_FILE, "utf-8"));
@@ -18,6 +20,13 @@ function writeProducts(products) {
     fs.writeFileSync(PRODUCTS_FILE, JSON.stringify(products, null, 2), "utf-8");
 }
 
+function writeCart(products) {
+    fs.writeFileSync(CART_FILE, JSON.stringify(products, null, 2), "utf-8");
+}
+
+function readCart() {
+    return JSON.parse(fs.readFileSync(CART_FILE, "utf-8"));
+}
 // Serve frontend static files
 app.use(express.static(__dirname));
 
@@ -34,7 +43,32 @@ app.post("/api/update-product", (req, res) => {
     product.stock = Math.max((product.stock || 0) - quantity, 0);
     product.order = (product.order || 0) + quantity;
 
+    const cart = readCart()
+    const item = cart.find(
+        item => item.id === id
+    )
+    if (item === undefined) {
+        cart.push({
+            id: id,
+            name: product.name,
+            image: product.image,
+            quantity: quantity,
+            price: product.price
+        })
+    }
+    else{
+        const itemIndex = cart.findIndex(
+            item => item.id === id
+        )
+        cart[itemIndex].quantity += quantity
+    }
+    writeCart(cart);
     writeProducts(products);
+    res.json({ success: true });
+});
+
+app.post("/api/remove-cart", (req, res) => {
+    writeCart(req.body);
     res.json({ success: true });
 });
 
